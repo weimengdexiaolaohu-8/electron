4149441c913d0a155862663328bb6a693c9d1f7d
Rice <1040776166@qq.com>
2026-09-02 23:05:34 +0800

    feat: 时钟挂件

--- index.ts
+++ index.ts
@@ -1,26 +1,26 @@
-import { app, BrowserWindow } from "electron";
+import { app, BrowserWindow, ipcMain } from "electron";
 import { fileURLToPath, format } from "node:url";
 import { dirname, join } from "node:path";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-let parent: BrowserWindow | null = null;
-let children: BrowserWindow | null = null;
 
 const createWindow = (config: any, parent?: any) => {
   const win = new BrowserWindow({
     width: config.width,
     height: config.height,
-    icon: "public/favicon.ico",
-    parent: parent ? parent : null,
-    alwaysOnTop: parent ? false : true,
+    transparent: true,
+    frame: false,
     webPreferences: {
       nodeIntegration: true, // 允许在渲染进程（在窗口）里面使用 node.js
       contextIsolation: false, // 关闭上下文隔离
       webviewTag: true, // 允许使用 <webview> 标签
     },
   });
+
+  win.setAlwaysOnTop(true, "pop-up-menu")
+  // win.setIgnoreMouseEvents(true); // 设置鼠标事件可以穿透
 
   win.loadFile(config.file);
 
@@ -30,31 +30,22 @@ const createWindow = (config: any, parent?: any) => {
 app.whenReady().then(() => {
   // 拼接 url
   const url1 = format({
-    pathname: join(__dirname, "window1/window1.html"),
+    pathname: join(__dirname, "window/index.html"),
   });
 
-  const url2 = format({
-    pathname: join(__dirname, "window2/window2.html"),
-  });
-
-  parent = createWindow({
-    width: 800,
-    height: 600,
+  createWindow({
+    width: 600,
+    height: 400,
     file: url1,
   });
-  children = createWindow(
-    {
-      width: 400,
-      height: 200,
-      file: url2,
-    },
-    parent,
-  );
+});
 
-  const { x, y, width } = parent?.getBounds()
-
-  children.setPosition(x+width+15, y)
-  children.show()
-
-  parent.setAlwaysOnTop(true, 'pop-up-menu')
+ipcMain.on("setIgnoreMouseEvent", (e, ingore, options?: { forward: boolean }) => {
+  console.log(ingore, options);
+  
+  // 通过 BrowserWindow.fromWebContents(e.sender) 拿到当前的窗口
+  // 等同于上面的 win
+  console.log(BrowserWindow.fromWebContents(e.sender));
+  
+  BrowserWindow.fromWebContents(e.sender)?.setIgnoreMouseEvents(ingore, options);
 });
